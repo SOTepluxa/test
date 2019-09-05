5f722173b9888bda80d3152e55167ee266a899f6
Sergei <sergsitepluha@gmail.com>
2019-09-05 09:42:00 +0300

    Это наш перый коммит

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+
+console.log(4/0);
+console.log('string'*5);
+let something;
+console.log(something)
+let person ={
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+console.log(person['name']);
+let arr = ['plum.png','orange.jpg']
+
+//console.log(arr[1])
+ 
+//alert("hello world");
+
+//let answer = confirm("are you here?");
+//console.log(answer);
+
+//let answer = +prompt("таптапт?", "Даа");
+//console.log(typeof(answer));
+//console.log("arr" + " 1143141");
+let incr = 10;
+    decr = 10;
+incr++;
+decr--;
